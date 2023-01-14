f5f2c56c91e374e8fb52900c319cfc9765253063
claudio <estebanclfe@gmail.com>
2023-01-14 13:05:22 -0300

    logout terminado

--- appchecklists/models.py
+++ appchecklists/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+#class Checklist(models.Model):
+#    id = models.AutoField(primary_key=True)
+#    fecha = models.DateTimeField(auto_now_add=True)
+    
