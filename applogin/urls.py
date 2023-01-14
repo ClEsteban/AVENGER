18f20dab6aae2f3d0814756ab3ad66292949c086
claudio <estebanclfe@gmail.com>
2023-01-14 09:34:24 -0300

    logout agregado

--- applogin/urls.py
+++ applogin/urls.py
@@ -4,7 +4,9 @@ from django.urls import include
 from AVENGER.views import *
 from applogin.views import *
 from appchecklists.views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('applogin/', vista_login, name="AVENGER-login"),
+    path('logout/', LogoutView.as_view(template_name="logout.html"), name="AVENGER-logout"),
 ]
