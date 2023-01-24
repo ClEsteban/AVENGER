ec463d748ee1a54c365eb007f20d15d9f10db842
claudio <estebanclfe@gmail.com>
2023-01-24 11:20:17 -0300

    base dir relativa

--- AVENGER/settings.py
+++ AVENGER/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'AVENGER.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r"C:\Users\cesteban\Downloads\PYTHON\AVENGER\AVENGER\templates"],
+        'DIRS': [os.path.join(BASE_DIR, r"AVENGER\templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
